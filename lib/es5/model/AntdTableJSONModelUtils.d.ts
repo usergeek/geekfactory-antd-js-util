13270e049286ee48b8f7c81de33818ee5b1db775
alexeyc <alexey.chirkov@rubylight.com>
2023-08-25 17:22:13 +0300

    0.0.2 - util methods

--- lib/es5/model/AntdTableJSONModelUtils.d.ts
+++ lib/es5/model/AntdTableJSONModelUtils.d.ts
@@ -1,8 +1,9 @@
+/// <reference types="react" />
 import { GenericFilterJSONModel, GenericPagingJSONModel, GenericSortingJSONModel } from "geekfactory-jsonmodel-util";
 import { FilterValue, SorterResult, TablePaginationConfig } from "antd/lib/table/interface";
 export declare const updateGenericPagingJSONModel: (pagingJSONModel: GenericPagingJSONModel, tablePagination: TablePaginationConfig) => void;
-export declare const updateGenericSortingJSONModel: <TableItemType>(sortingJSONModel: GenericSortingJSONModel, sorter: SorterResult<TableItemType>) => void;
-export declare const updateGenericFilterJSONModel: <TableItemType, FilterItemName>(filterJSONModel: GenericFilterJSONModel<FilterItemName, string>, filter: Record<string, FilterValue | null>) => void;
+export declare const updateGenericSortJSONModel: <TableItemType, SortKey extends string>(sortJSONModel: GenericSortingJSONModel<SortKey>, sorter: SorterResult<TableItemType> | SorterResult<TableItemType>[] | null) => void;
+export declare const updateGenericFilterJSONModel: <FilterItemName, FilterItemValue>(filterJSONModel: GenericFilterJSONModel<FilterItemName, FilterItemValue>, filter: Record<string, FilterValue | null>) => void;
 export declare const getTablePaginationConfigFromGenericPagingJSONModel: (pagingJSONModel: GenericPagingJSONModel, defaultPaginationConfig: TablePaginationConfig) => TablePaginationConfig;
-export declare const getTableSorterFromGenericPagingJSONModel: <TableItemType>(sortingJSONModel: GenericSortingJSONModel) => SorterResult<TableItemType>;
-export declare const getTableFilterFromGenericPagingJSONModel: (filterJSONModel: GenericFilterJSONModel<string, string>) => Record<string, FilterValue | null>;
+export declare const getTableSorterFromGenericSortJSONModel: <TableItemType, SortKey extends string>(sortJSONModel: GenericSortingJSONModel<SortKey>) => SorterResult<TableItemType> | SorterResult<TableItemType>[];
+export declare const getTableFilterFromGenericPagingJSONModel: <FilterItemName extends string, FilterItemValue extends import("react").Key>(filterJSONModel: GenericFilterJSONModel<FilterItemName, FilterItemValue>) => Record<string, FilterValue | null>;
--- src/model/AntdTableJSONModelUtils.ts
+++ src/model/AntdTableJSONModelUtils.ts
@@ -1,28 +1,57 @@
-import {GenericFilterJSONModel, GenericFilterJSONModelArrayValue, GenericPagingJSONModel, GenericSortingJSONModel, SortOrder} from "geekfactory-jsonmodel-util";
-import {FilterValue, SorterResult, TablePaginationConfig} from "antd/lib/table/interface";
+import {GenericFilterJSONModel, GenericFilterJSONModelArrayValue, GenericPagingJSONModel, GenericSortingItemJSONModel, GenericSortingItemJSONModelSerializedState, GenericSortingJSONModel, GenericSortingJSONModelSerializedState, SORT_ODER_ASC, SORT_ODER_DESC, SortOrder} from "geekfactory-jsonmodel-util";
+import {FilterValue, Key, Key as AntdKey, SorterResult, SortOrder as AntdSortOrder, TablePaginationConfig} from "antd/lib/table/interface";
 import _ from "lodash"
 
 export const updateGenericPagingJSONModel = (pagingJSONModel: GenericPagingJSONModel, tablePagination: TablePaginationConfig) => {
     pagingJSONModel.setValue(tablePagination.current, tablePagination.pageSize)
 }
 
-export const updateGenericSortingJSONModel = <TableItemType, >(sortingJSONModel: GenericSortingJSONModel, sorter: SorterResult<TableItemType>) => {
-    const sortOrder: SortOrder | undefined = sorter.order == "ascend" || sorter.order == "descend" ? sorter.order : undefined
-    const sortKey: string | undefined = !_.isEmpty(sorter.columnKey) ? `${sorter.columnKey}` : undefined
+export const updateGenericSortJSONModel = <TableItemType, SortKey extends string>(sortJSONModel: GenericSortingJSONModel<SortKey>, sorter: SorterResult<TableItemType> | SorterResult<TableItemType>[] | null) => {
+    if (sorter != undefined) {
+        if (_.isArray(sorter)) {
+            console.log("updateGenericSortJSONModel: array", sorter);
+            const values = _.compact(_.map(sorter, (sorterItem) => {
+                return getSortValuesJSONModelSerializedStateFromSorter<TableItemType, SortKey>(sorterItem)
+            }))
+            console.log("updateGenericSortJSONModel: values", values);
+            if (values.length > 0) {
+                sortJSONModel.getSortValuesJSONModel().setValue(values)
+                return
+            }
+        } else {
+            console.log("updateGenericSortJSONModel: single", sorter);
+            const serializedStateFromSorter = getSortValuesJSONModelSerializedStateFromSorter<TableItemType, SortKey>(sorter);
+            console.log("updateGenericSortJSONModel: serializedStateFromSorter", serializedStateFromSorter);
+            if (serializedStateFromSorter != undefined) {
+                sortJSONModel.getSortValuesJSONModel().setValue([serializedStateFromSorter])
+                return
+            }
+        }
+    }
+    console.log("updateGenericSortJSONModel: reset");
+    sortJSONModel.getSortValuesJSONModel().reset()
+}
 
+const getSortValuesJSONModelSerializedStateFromSorter = <TableItemType, SortKey extends string>(sorter: SorterResult<TableItemType>): GenericSortingItemJSONModelSerializedState<SortKey> | undefined => {
+    // {column: undefined, order: undefined, field: undefined, columnKey: 'proposalCreatedTime'}
+    const sortOrder: SortOrder | undefined = antdSortOrderToJSONModelSortOrder(sorter.order)
+    const sortKey: string | undefined = antdSortKeyToJSONModelSortKey(sorter.columnKey)
+    console.log("getSortValuesJSONModelSerializedStateFromSorter: ", {sorter, sortOrder, sortKey});
     if (sortOrder != undefined && sortKey != undefined) {
-        sortingJSONModel.getSortKeyJSONModel().setValue(sortKey)
-        sortingJSONModel.getSortOrderJSONModel().setValue(sortOrder)
+        const sortItemJSONModel = new GenericSortingItemJSONModel<SortKey>();
+        sortItemJSONModel.getKeyJSONModel().setValue(sortKey)
+        sortItemJSONModel.getOrderJSONModel().setValue(sortOrder)
+        return sortItemJSONModel.serialize()
     }
 }
 
-export const updateGenericFilterJSONModel = <TableItemType, FilterItemName>(filterJSONModel: GenericFilterJSONModel<FilterItemName, string>, filter: Record<string, FilterValue | null>) => {
-    const values: Array<GenericFilterJSONModelArrayValue<FilterItemName, string>> = []
+export const updateGenericFilterJSONModel = <FilterItemName, FilterItemValue>(filterJSONModel: GenericFilterJSONModel<FilterItemName, FilterItemValue>, filter: Record<string, FilterValue | null>) => {
+    const values: Array<GenericFilterJSONModelArrayValue<FilterItemName, FilterItemValue>> = []
     _.each(filter, (filterValue, filterKey) => {
         if (filterValue != undefined && _.isArray(filterValue) && !_.isEmpty(filterValue)) {
             values.push({
                 name: filterKey as unknown as FilterItemName,
-                values: filterValue as Array<string>,
+                values: filterValue as unknown as Array<FilterItemValue>,
             })
         }
     })
@@ -32,19 +61,33 @@ export const updateGenericFilterJSONModel = <TableItemType, FilterItemName>(filt
 export const getTablePaginationConfigFromGenericPagingJSONModel = (pagingJSONModel: GenericPagingJSONModel, defaultPaginationConfig: TablePaginationConfig): TablePaginationConfig => {
     return {
         ...defaultPaginationConfig,
-        pageSize: pagingJSONModel.getPageSize().getValue(),
-        current: pagingJSONModel.getCurrentPage().getValue(),
+        pageSize: pagingJSONModel.getLimitValue(),
+        current: pagingJSONModel.getPageValue(),
     }
 }
 
-export const getTableSorterFromGenericPagingJSONModel = <TableItemType, >(sortingJSONModel: GenericSortingJSONModel): SorterResult<TableItemType> => {
-    return {
-        columnKey: sortingJSONModel.getSortKeyJSONModel().getValue(),
-        order: sortingJSONModel.getSortOrderJSONModel().getValue(),
+export const getTableSorterFromGenericSortJSONModel = <TableItemType, SortKey extends string>(sortJSONModel: GenericSortingJSONModel<SortKey>): SorterResult<TableItemType> | SorterResult<TableItemType>[] => {
+    const value: GenericSortingJSONModelSerializedState<SortKey> | undefined = sortJSONModel.getSortValuesJSONModel().getValue();
+    if (value != undefined && value.length > 0) {
+        if (value.length > 1) {
+            return _.map(value, (sortItem) => {
+                return {
+                    columnKey: sortItem.key,
+                    order: jsonModelSortOrderToAntdSortOrder(sortItem.order),
+                }
+            })
+        } else {
+            const sortItem = value[0]
+            return {
+                columnKey: sortItem.key,
+                order: jsonModelSortOrderToAntdSortOrder(sortItem.order),
+            }
+        }
     }
+    return {}
 }
 
-export const getTableFilterFromGenericPagingJSONModel = (filterJSONModel: GenericFilterJSONModel<string, string>): Record<string, FilterValue | null> => {
+export const getTableFilterFromGenericPagingJSONModel = <FilterItemName extends string, FilterItemValue extends Key>(filterJSONModel: GenericFilterJSONModel<FilterItemName, FilterItemValue>): Record<string, FilterValue | null> => {
     const filter: Record<string, FilterValue | null> = {}
     filterJSONModel.getFilterValuesJSONModel().getValue()?.forEach((filterValue) => {
         const name = filterValue.name
@@ -54,4 +97,28 @@ export const getTableFilterFromGenericPagingJSONModel = (filterJSONModel: Generi
         }
     })
     return filter
+}
+
+const antdSortOrderToJSONModelSortOrder = (antdSortOrder?: AntdSortOrder): SortOrder | undefined => {
+    if (antdSortOrder != undefined) {
+        if (antdSortOrder == "ascend") {
+            return SORT_ODER_ASC
+        } else if (antdSortOrder == "descend") {
+            return SORT_ODER_DESC
+        }
+    }
+}
+
+const jsonModelSortOrderToAntdSortOrder = (sortOrder?: string): AntdSortOrder | undefined => {
+    if (sortOrder != undefined) {
+        if (sortOrder == SORT_ODER_ASC) {
+            return "ascend"
+        } else if (sortOrder == SORT_ODER_DESC) {
+            return "descend"
+        }
+    }
+}
+
+const antdSortKeyToJSONModelSortKey = (antdSortKey?: AntdKey): string | undefined => {
+    return !_.isEmpty(antdSortKey) ? `${antdSortKey}` : undefined
 }
